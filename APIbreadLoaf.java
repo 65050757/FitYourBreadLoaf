fad0b02f3a7f065daa673318dde8a9e4af77f9c7
Jane <82717630+65050757@users.noreply.github.com>
2023-10-25 16:58:06 +0700

    Create APIbreadLoaf.java
    โครงคร่าวๆ

---
+++ APIbreadLoaf.java
@@ -0,0 +1,40 @@
+public class APIbreadLoaf extends BreadLoaf implements typeInterface{
+
+    APIbreadLoaf(){
+        isConnected();
+    }
+
+    private static void isConnected(){
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+
+    @Override
+    public void importName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'importName'");
+    }
+
+    @Override
+    public void importIngredient() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'importIngredient'");
+    }
+
+    @Override
+    public void importProcess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'importProcess'");
+    }
+
+    @Override
+    public void showDescription() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showDescription'");
+    }
+    
+}
