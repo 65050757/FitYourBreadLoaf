05b43b95616368162a7dac720d88d6cf93e501ca
Jane <82717630+65050757@users.noreply.github.com>
2023-10-25 15:50:32 +0700

    Update Ingredient.java

--- Code/Ingredient.java
+++ Code/Ingredient.java
@@ -1,3 +1,14 @@
 public class Ingredient{
-    
+    private String name;
+    private double quantityIngram; //gram in unit
+
+    public Ingredient(String name,double quantity){
+        this.name = name;
+        this.quantityIngram = quantity;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + quantityIngram + " gram";
+    }
 }
